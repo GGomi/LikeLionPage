174b8628a8a67799a48125fb46af7128c58bf160
JeongMin <dhkdn4042@naver.com>
2017-12-04 19:47:01 +0900

    Delete select.php

--- www/select.php
+++
@@ -1,6 +0,0 @@
-<?php
-  $conn = mysqli_connect('aliseondb.ckrbe4nzcuuc.ap-northeast-2.rds.amazonaws.com', 'hansonmaria', 'ssh1566010', 'aliseonmedia');
-  $getlist_sql = "SELECT *from vod_queue";
-  $result_get= $conn->query($getlist_sql);
-?>
-
